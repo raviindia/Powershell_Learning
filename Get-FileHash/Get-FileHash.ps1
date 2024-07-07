844bcc1483e4c6e87f1734699dd99d06caa7a011
Ravi Prakash <ravi00prakash@gmail.com>
2024-07-07 07:47:58 +0530

    Update Get-FileHash.ps1

--- Get-FileHash/Get-FileHash.ps1
+++ Get-FileHash/Get-FileHash.ps1
@@ -13,17 +13,7 @@ $filePath = Read-Host "File Path with file name and extension"
 #Taking Algorithm as input from the user.
 $Algorithm = Read-Host 'What algorithm is needed? Accepted values: SHA1, SHA256, SHA384, SHA512, MD5'
 
-#$directoryPath = Split-Path -Path $filePath
-#Write-Output $directoryPath
-
-#Write-Output $filePath
-
-#running Get-FileHash command and printing out the hash value in formatted list.
-#Get-FileHash $filePath -Algorithm $Algorithm | Format-List
-
-#Read-Host -Prompt "Press Enter to exit"
-#powershell
-#pause
+#running Get-FileHash command with if function and printing out the hash value in formatted list.
 if ( $Algorithm -eq "" )
 {
     Get-FileHash $filePath | Format-List
@@ -34,20 +24,3 @@ else
 }
 
 Read-Host -Prompt "Press Enter to exit"
-#-----------------------------------
-
-#$stringAsStream = [System.IO.MemoryStream]::new()
-#$writer = [System.IO.StreamWriter]::new($stringAsStream)
-#$writer.write("Hello world")
-#$writer.Flush()
-#$stringAsStream.Position = 0
-#Get-FileHash -InputStream $stringAsStream | Select-Object Hash
-#
-#Hash
-#----
-#64EC88CA00B268E5BA1A35678A1B5316D212F4F366B2477232534A8AECA37F3C
-
-
-#Hash
-#----
-#64EC88CA00B268E5BA1A35678A1B5316D212F4F366B2477232534A8AECA37F3C
